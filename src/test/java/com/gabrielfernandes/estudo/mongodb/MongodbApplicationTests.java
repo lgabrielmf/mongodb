9212fcff1e364bd94d682208308b025450699ca6
Luis Gabriel de M Fernandes <lgabrielmf@gmail.com>
2022-08-14 11:38:09 -0300

    Initial commit

---
+++ src/test/java/com/gabrielfernandes/estudo/mongodb/MongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielfernandes.estudo.mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
